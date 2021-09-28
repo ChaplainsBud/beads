fa253d2f642dee9190945104fb2265034c7fced5
Matt Leroe <matthew.robert.leroe@gmail.com>
2021-09-28 19:07:15 -0400

    Login Page with Firebase: https://www.youtube.com/watch?v=1HN7usMROt8&t=1125s

---
+++ beads/View Controllers/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  beads
+//
+//  Created by Matthew Leroe on 9/28/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // developing login page
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
